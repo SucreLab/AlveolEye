03ee8fd3240342d53ee07b4d91b61353acbdd0db
Quooooooookka <josephhirsh9@gmail.com>
2025-01-19 18:28:15 -0500

    Improvements, combined map, new colors

--- src/alveoleye/lungcv/confidence_maps.py
+++ src/alveoleye/lungcv/confidence_maps.py
@@ -1,72 +1,118 @@
 import argparse
+import os
 import torch
 from model_operations import init_trained_model, run_prediction
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
+import time
 
 
-def main(model_path, image_path, output_path=None, colorbar_orientation="vertical"):
+def create_heatmaps(model_path, image_path, output_dir, colorbar_orientation="vertical"):
     model = init_trained_model(model_path)
     prediction = run_prediction(image_path, model)
     masks = prediction["masks"]
-    labels = prediction["labels"]  # Assuming labels are provided as a tensor or list
-
-    print(prediction)
-
+    labels = prediction["labels"]
     original_image = np.array(Image.open(image_path).convert("RGB"))
+    image_name = os.path.splitext(os.path.basename(image_path))[0]
 
-    # Create confidence maps for class 1 and class 2
-    combined_heatmap_class1 = torch.stack(
-        [mask.squeeze() for mask, label in zip(masks, labels) if label == 1]
-    ).sum(dim=0).detach().numpy()
+    class_colormaps = {
+        1: 'Blues',
+        2: 'Reds',
+    }
 
-    combined_heatmap_class2 = torch.stack(
-        [mask.squeeze() for mask, label in zip(masks, labels) if label == 2]
-    ).sum(dim=0).detach().numpy()
+    heatmaps = {}
+    for class_label, colormap in class_colormaps.items():
+        combined_heatmap = create_combined_heatmap(masks, labels, class_label)
+        heatmaps[class_label] = combined_heatmap
+        save_heatmap_image(original_image, combined_heatmap, colormap, image_name, class_label, output_dir,
+                           colorbar_orientation)
 
-    # Create the figure and axes
-    if colorbar_orientation == "vertical":
-        fig, axs = plt.subplots(1, 2, figsize=(8, 4), gridspec_kw={"wspace": 0.1, "right": 0.85})
-        cbar_ax = fig.add_axes([0.88, 0.25, 0.02, 0.5])  # Balanced dimensions for vertical
-    elif colorbar_orientation == "horizontal":
-        fig, axs = plt.subplots(1, 2, figsize=(8, 4), gridspec_kw={"wspace": 0.1, "bottom": 0.2})
-        cbar_ax = fig.add_axes([0.3, 0.12, 0.4, 0.02])  # Balanced dimensions for horizontal
+    save_combined_overlap_image(original_image, heatmaps, image_name, output_dir, colorbar_orientation)
 
-    # Class 1 heatmap
-    axs[0].imshow(original_image, alpha=1.0)
-    axs[0].imshow(combined_heatmap_class1, cmap='hot', alpha=0.75, interpolation='nearest')
-    axs[0].set_title('Confidence Map: Class 1', fontsize=10)
-    axs[0].axis('off')
 
-    # Class 2 heatmap
-    axs[1].imshow(original_image, alpha=1.0)
-    axs[1].imshow(combined_heatmap_class2, cmap='hot', alpha=0.75, interpolation='nearest')
-    axs[1].set_title('Confidence Map: Class 2', fontsize=10)
-    axs[1].axis('off')
+def create_combined_heatmap(masks, labels, class_label):
+    return torch.stack([mask.squeeze() for mask, label in zip(masks, labels) if label == class_label]).max(dim=0)[
+        0].detach().numpy()
 
-    # Colorbar
-    cbar = fig.colorbar(
-        plt.cm.ScalarMappable(cmap='hot'),
-        cax=cbar_ax,
-        orientation=colorbar_orientation
-    )
+
+def save_heatmap_image(original_image, combined_heatmap, colormap, image_name, class_label, output_dir,
+                       colorbar_orientation):
+    plt.figure(figsize=(8, 6))
+    plt.imshow(original_image, alpha=1.0)
+    heatmap = plt.imshow(combined_heatmap, cmap=colormap, alpha=0.75, interpolation='nearest', vmin=0, vmax=1)
+    plt.axis('off')
+    cbar = plt.colorbar(heatmap, orientation=colorbar_orientation, ticks=np.linspace(0, 1, 6))
     cbar.set_label('Confidence Level', fontsize=10)
+    class_output_dir = os.path.join(output_dir, image_name)
+    os.makedirs(class_output_dir, exist_ok=True)
+    plt.savefig(f"{class_output_dir}/{image_name}.{class_label}.png", bbox_inches='tight')
+    plt.close()
 
-    # Save or display the figure
-    if output_path:
-        plt.savefig(output_path, bbox_inches='tight')
-    else:
-        plt.show()
+
+def save_combined_overlap_image(original_image, heatmaps, image_name, output_dir, colorbar_orientation):
+    plt.figure(figsize=(8, 6))
+    plt.imshow(original_image, alpha=1.0)
+    plt.imshow(heatmaps[1], cmap='Blues', alpha=0.5, interpolation='nearest', vmin=0, vmax=1)
+    plt.imshow(heatmaps[2], cmap='Reds', alpha=0.5, interpolation='nearest', vmin=0, vmax=1)
+    plt.axis('off')
+    cbar1 = plt.colorbar(plt.imshow(heatmaps[1], cmap='Blues', alpha=0.5, interpolation='nearest', vmin=0, vmax=1),
+                         orientation=colorbar_orientation, ticks=np.linspace(0, 1, 6))
+    cbar1.set_label('Confidence Level Class 1', fontsize=10)
+    cbar2 = plt.colorbar(plt.imshow(heatmaps[2], cmap='Reds', alpha=0.5, interpolation='nearest', vmin=0, vmax=1),
+                         orientation=colorbar_orientation, ticks=np.linspace(0, 1, 6))
+    cbar2.set_label('Confidence Level Class 2', fontsize=10)
+    class_output_dir = os.path.join(output_dir, image_name)
+    plt.savefig(f"{class_output_dir}/{image_name}_combined.png", bbox_inches='tight')
+    plt.close()
+
+
+def process_images_in_directory(directory_path, model_path, output_dir, colorbar_orientation="vertical"):
+    output_dir = os.path.join(output_dir, "confidence_maps")
+    image_files = [image_name for image_name in os.listdir(directory_path) if
+                   image_name.lower().endswith(('.png', '.jpg', '.jpeg'))]
+    total_images = len(image_files)
+
+    print(f"[+] Producing confidence maps for {total_images}")
+
+    for idx, image_name in enumerate(image_files):
+        image_path = os.path.join(directory_path, image_name)
+        create_heatmaps(model_path, image_path, output_dir, colorbar_orientation)
+        print(f"[+] Processed {idx + 1}/{total_images} images", end="\r")
+
+    print(f"[+] Produced {total_images}/{total_images} confidence maps")
+
+
+def main(input_dir, model_path, output_dir, colorbar_orientation="vertical"):
+    start_time = time.time()
+    process_images_in_directory(input_dir, model_path, output_dir, colorbar_orientation)
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"Total Processing Time: {elapsed_time:.2f} seconds")
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Overlay heatmaps for different classes on an image")
-    parser.add_argument("model_path", type=str, help="Path to the trained model file")
-    parser.add_argument("image_path", type=str, help="Path to the input image")
-    parser.add_argument("--output_path", type=str, help="Path to save the output image (optional)")
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+
+    default_input_dir = os.path.abspath(os.path.join(script_dir, "../data"))
+    default_model_path = os.path.abspath(os.path.join(script_dir, "../../../data/default.pth"))
+
+    parser = argparse.ArgumentParser(description="Generate heatmaps for each image in a directory")
+    parser.add_argument("--input_dir", type=str, default=default_input_dir,
+                        help="Path to the directory containing images (default: ../../data)")
+    parser.add_argument("--model_path", type=str, default=default_model_path,
+                        help="Path to the trained model file (default: ../../../../data/default.pth)")
+    parser.add_argument("--output_dir", type=str, required=True,
+                        help="Directory to save the output images (required)")
     parser.add_argument("--colorbar_orientation", type=str, choices=["vertical", "horizontal"],
-                        default="horizontal", help="Orientation of the colorbar (default: vertical)")
+                        default="vertical", help="Orientation of the colorbar (default: vertical)")
 
     args = parser.parse_args()
-    main(args.model_path, args.image_path, args.output_path, args.colorbar_orientation)
+
+    print(f"Running with the following arguments:\n"
+          f"Input Directory: {args.input_dir}\n"
+          f"Model Path: {args.model_path}\n"
+          f"Output Directory: {args.output_dir}\n"
+          f"Colorbar Orientation: {args.colorbar_orientation}")
+
+    main(args.input_dir, args.model_path, args.output_dir, args.colorbar_orientation)
