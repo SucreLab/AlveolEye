038061c820eea8c8ced0911554bf1995c16b386b
Quooooooookka <josephhirsh9@gmail.com>
2025-04-08 16:28:06 -0400

    Added AI bypass check box graphics (without functionality yet)

--- src/alveoleye/_boxes.py
+++ src/alveoleye/_boxes.py
@@ -11,7 +11,6 @@ from alveoleye._workers import ProcessingWorker, PostprocessingWorker, Assessmen
 
 import alveoleye._gui_creator as gui_creator
 import alveoleye._layers_editor as layers_editor
-import alveoleye._rules as rules
 
 
 class ProcessingActionBox(ActionBox):
@@ -23,7 +22,10 @@ class ProcessingActionBox(ActionBox):
         self.image = None
 
         self.import_image_line_edit = None
+        self.import_weights_button_and_line_edit_layout = None
+        self.use_ai_check_box = None
         self.import_weights_line_edit = None
+        self.confidence_threshold_label_and_spin_box_layout = None
         self.confidence_threshold_spin_box = None
 
         self.box_id = 1
@@ -54,6 +56,14 @@ class ProcessingActionBox(ActionBox):
             self.on_import_image_press,
             self.box_config_data["EMPTY_PATH_LINE_EDIT_TEXT"]
         )
+
+        horizontal_line = gui_creator.create_horizontal_line_widget()
+
+        use_ai_check_box = gui_creator.create_check_box_widget(
+            self.box_config_data["USE_AI_CHECK_BOX_TEXT"],
+            self.rules_engine.evaluate_rules,
+            self.box_config_data["USE_AI_CHECK_BOX_TOOLTIP_TEXT"])
+
         import_weights_button_and_line_edit = gui_creator.create_button_and_line_edit_layout(
             self.box_config_data["IMPORT_WEIGHTS_BUTTON_TEXT"],
             self.box_config_data["IMPORT_WEIGHTS_BUTTON_TOOLTIP_TEXT"],
@@ -75,10 +85,15 @@ class ProcessingActionBox(ActionBox):
         confidence_threshold_label_and_spin_box_layout = confidence_threshold_label_and_spin_box[0]
 
         self.import_image_line_edit = import_image_button_and_line_edit[2]
+        self.import_weights_button_and_line_edit_layout = import_weights_button_and_line_edit_layout
+        self.use_ai_check_box = use_ai_check_box
         self.import_weights_line_edit = import_weights_button_and_line_edit[2]
         self.confidence_threshold_spin_box = confidence_threshold_label_and_spin_box[2]
+        self.confidence_threshold_label_and_spin_box_layout = confidence_threshold_label_and_spin_box_layout
 
         ui_elements = [import_image_button_and_line_edit_layout,
+                       horizontal_line,
+                       use_ai_check_box,
                        import_weights_button_and_line_edit_layout,
                        confidence_threshold_label_and_spin_box_layout]
 
@@ -99,6 +114,13 @@ class ProcessingActionBox(ActionBox):
                                    lambda: alveoleye._gui_creator.toggle(False, self.import_image_line_edit))
         self.rules_engine.add_rule(lambda: ActionBox.import_paths["image"] is not None,
                                    lambda: alveoleye._gui_creator.toggle(True, self.import_image_line_edit))
+
+        self.rules_engine.add_rule(lambda: self.use_ai_check_box.isChecked(),
+                                   lambda: alveoleye._gui_creator.toggle(True, [self.import_weights_button_and_line_edit_layout,
+                                                                                self.confidence_threshold_label_and_spin_box_layout]))
+        self.rules_engine.add_rule(lambda: not self.use_ai_check_box.isChecked(),
+                                   lambda: alveoleye._gui_creator.toggle(False, [self.import_weights_button_and_line_edit_layout,
+                                                                                 self.confidence_threshold_label_and_spin_box_layout]))
 
         super().create_ui_rules()
 
@@ -620,6 +642,3 @@ class ExportActionBox(ActionBox):
 
     def on_results_ready(self, wrapped_data, extension):
         pass
-
-
-
