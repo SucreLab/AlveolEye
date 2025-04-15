c832a3166a2b47943e925c6758285757bd2af0b8
Quooooooookka <josephhirsh9@gmail.com>
2025-04-15 17:48:16 -0400

    Started tooltip on viewer feature

--- src/alveoleye/_layers_editor.py
+++ src/alveoleye/_layers_editor.py
@@ -1,4 +1,9 @@
 from napari.utils.colormaps import DirectLabelColormap
+from napari import Viewer
+import numpy as np
+from qtpy.QtWidgets import QLabel, QSizePolicy
+from qtpy.QtCore import Qt
+import warnings
 
 
 def get_layer_by_name(napari_viewer, layer_name):
@@ -33,9 +38,32 @@ def update_layers(napari_viewer, layer_name, layer_data, color_dict, is_labelmap
         color_dict[None] = [0, 0, 0]
         colormap = DirectLabelColormap(color_dict=color_dict)
 
-        napari_viewer.add_labels(layer_data, colormap=colormap, opacity=1.0, name=layer_name)
+        layer = napari_viewer.add_labels(layer_data, colormap=colormap, opacity=1.0, name=layer_name)
+        layer.mouse_move_callbacks.append(on_mouse_move_factory(napari_viewer))
         napari_viewer.layers[layer_name].editable = True
         return
 
     layer_data_rgb = layer_data[:, :, ::-1]
-    napari_viewer.add_image(layer_data_rgb, name=layer_name)
+    layer = napari_viewer.add_image(layer_data_rgb, name=layer_name)
+    layer.mouse_move_callbacks.append(on_mouse_move_factory(napari_viewer))
+
+
+def on_mouse_move_factory(napari_viewer):
+    def on_mouse_move(layer, event):
+        tooltip = QLabel(napari_viewer.window.qt_viewer.parent())
+        tooltip.setWindowFlags(Qt.Window | Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint)
+        tooltip.setAttribute(Qt.WA_ShowWithoutActivating)
+        tooltip.setFixedSize(20, 20)
+        tooltip.setAlignment(Qt.AlignCenter)
+        tooltip.sizePolicy().setHorizontalPolicy(QSizePolicy.Fixed)
+        tooltip.setStyleSheet("color: black")
+        tooltip.show()
+
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            pos = napari_viewer.window.qt_viewer.cursor().pos()
+            tooltip.move(pos.x()+20, pos.y()+20)
+            val = layer.get_value(event.position)
+            tooltip.setText(str(val) if val is not None else "-")
+
+    return on_mouse_move
